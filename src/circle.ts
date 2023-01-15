fa47a2041fd1069f0ed9f2d92a57eaf57e97a67d
HelgaAthame <athamethedarkest@gmail.com>
2023-01-15 19:38:10 +0300

    feat: implement circle drawing

---
+++ src/circle.ts
@@ -0,0 +1,16 @@
+import { Button, mouse, MouseClass, Point, straightTo } from "@nut-tree/nut-js";
+
+export const drawCircle = async ( r: number ) => {
+  let { x, y } = await mouse.getPosition();
+  x -= r;
+  mouse.config.mouseSpeed = 1000;
+  await mouse.pressButton(Button.LEFT);
+  for ( let i = 0; i < 360; i++ ) {
+    const angle = ( i * Math.PI ) / 180;
+    const curX = x + r * Math.cos(angle);
+    const curY = y + r * Math.sin(angle);
+    const point = new Point(curX, curY);
+    await mouse.move(straightTo(point))
+  }
+  await mouse.releaseButton(Button.LEFT);
+}
--- src/handler.ts
+++ src/handler.ts
@@ -1,11 +1,15 @@
 import WebSocket from "ws";
 import { mouseMove } from './mouseMove.js';
+import { drawCircle } from './circle.js';
 
 export const handlerFunc = (data: WebSocket.RawData) => {
   console.log(`we received ${data}`);
   const string = data.toString();
   if (string.startsWith('mouse')) {
     return mouseMove(string);
+  }
+  if (string.startsWith('draw_circle')) {
+    drawCircle(Number(string.slice(11)));
   }
   return 5;
 }
