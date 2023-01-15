8d8b52827b8eb489e343aa29c497928c3573955d
HelgaAthame <athamethedarkest@gmail.com>
2023-01-15 19:55:40 +0300

    feat: implement rectangle drawing

--- src/handler.ts
+++ src/handler.ts
@@ -2,6 +2,7 @@ import WebSocket from "ws";
 import { mouseMove } from './mouseMove.js';
 import { drawCircle } from './circle.js';
 import { drawSquare } from './square.js';
+import { drawRectangle } from './rectangle.js';
 
 export const handlerFunc = (data: WebSocket.RawData) => {
   console.log(`we received ${data}`);
@@ -14,6 +15,10 @@ export const handlerFunc = (data: WebSocket.RawData) => {
   }
   if (string.startsWith('draw_square')) {
     drawSquare(Number(string.slice(11)));
+  }
+  if (string.startsWith('draw_rectangle')) {
+    const arr = string.split(' ').map( el => Number(el) );
+    drawRectangle(arr[1], arr[2]);
   }
   return 5;
 }
